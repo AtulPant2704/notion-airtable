5696d9ce32953c8fd9ad4ee19f6726e834128f6c
AtulPant2704 <atulpant1999@gmail.com>
2022-07-08 12:45:03 +0530

    feat: card modal component

---
+++ src/components/CardModal/CardModal.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useData } from "context";
+import "./CardModal.css";
+
+const CardModal = ({
+  setDisplayCardModal,
+  id,
+  title,
+  description,
+  status,
+  listId,
+}) => {
+  const { state, dispatch } = useData();
+  const [newCard, setNewCard] = useState({
+    title: "",
+    description: "",
+    status: "",
+  });
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setNewCard((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <>
+      <div
+        className="backdrop"
+        onClick={() => setDisplayCardModal(false)}
+      ></div>
+      <div className="card-modal">
+        <div className="card-header">
+          <input
+            name="title"
+            className="card-modal-title"
+            value={newCard.title || title}
+            onChange={inputHandler}
+          ></input>
+          <label htmlFor="card-status">Status</label>
+          <select
+            id="card-status"
+            name="status"
+            className="card-status-select"
+            onChange={inputHandler}
+          >
+            <option value={newCard.status || status}>{status}</option>
+            {state.map((list) => (
+              <option key={list.id} value={list.name}>
+                {list.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <textarea
+          className="card-description"
+          name="description"
+          rows="5"
+          cols="33"
+          placeholder="Enter description for card"
+          value={newCard.description || description}
+          onChange={inputHandler}
+        ></textarea>
+        <div className="card-footer">
+          <button
+            onClick={() =>
+              dispatch({
+                type: "EDIT_CARD",
+                payload: { listId, cardId: id, cardData: newCard },
+              })
+            }
+          >
+            Save
+          </button>
+          <button
+            onClick={() =>
+              dispatch({
+                type: "DELETE_CARD",
+                payload: { listId, cardId: id },
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { CardModal };
