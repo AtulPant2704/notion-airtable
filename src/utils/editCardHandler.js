d993f711fdb04dab76497e7230c5bf4d9a124fba
AtulPant2704 <atulpant1999@gmail.com>
2022-07-08 21:43:42 +0530

    fix: edit card handler

--- src/utils/editCardHandler.js
+++ src/utils/editCardHandler.js
@@ -1,8 +1,11 @@
 const editCardHandler = (data, listId, cardId, cardData) => {
   let newData = data.map((list) =>
     list.name === cardData.status
-      ? { ...list, cards: [...list.cards, cardData] }
-      : list.id === listId
+      ? {
+          ...list,
+          cards: [...list.cards.filter((card) => card.id !== cardId), cardData],
+        }
+      : list.id === listId && list.name !== cardData.status
       ? { ...list, cards: list.cards.filter((card) => card.id !== cardId) }
       : list
   );
